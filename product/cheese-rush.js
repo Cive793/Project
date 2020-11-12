eaedbfa7f45428f214d4d59210c495e9c0a4d79d
peter <peter.stastny7@gmail.com>
2020-11-12 13:55:14 +0100

    rolling animation

--- product/cheese-rush.js
+++ product/cheese-rush.js
@@ -1,1 +1,11 @@
+window.addEventListener("load", startGame);
 
+function startGame() {
+    console.log("function startGame");
+    document.querySelector("#container5").classList.add("falling");
+    document.querySelector("#container5").classList.add("position5");
+    document.querySelector("#container6").classList.add("falling");
+    document.querySelector("#container6").classList.add("position6");
+    document.querySelector("#cheese1").classList.add("rolling");
+
+}
