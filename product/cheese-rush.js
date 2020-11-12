2fec192bffa3f7f44093fa31a5b625dcfe56b756
peter <peter.stastny7@gmail.com>
2020-11-12 14:02:20 +0100

    rolling animations assigned

--- product/cheese-rush.js
+++ product/cheese-rush.js
@@ -4,8 +4,13 @@ function startGame() {
     console.log("function startGame");
     document.querySelector("#container5").classList.add("falling");
     document.querySelector("#container5").classList.add("position5");
+    document.querySelector("#cheese1").classList.add("rolling");
     document.querySelector("#container6").classList.add("falling");
     document.querySelector("#container6").classList.add("position6");
-    document.querySelector("#cheese1").classList.add("rolling");
-
+    document.querySelector("#cheese2").classList.add("rolling");
+    document.querySelector("#container7").classList.add("falling");
+    document.querySelector("#container7").classList.add("position7");
+    document.querySelector("#mousetrap1").classList.add("rolling");
+    document.querySelector("#container4").classList.add("horizontal");
+    document.querySelector("#container4").classList.add("position4");
 }
